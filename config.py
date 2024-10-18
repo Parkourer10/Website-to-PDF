7d583e2a4e401d9a6ecddda3973211dba0d48bca
Park <168292059+Parkourer10@users.noreply.github.com>
2024-10-18 22:18:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BASE_URL='https://minecraft.wiki/' #website url
+BASE_URL='' #website url
 BATCH_SIZE=10 #making batches of pdfs to combine to a large pdf. change it to a higher value or lower value depending on your system.
 NUM_CORES=os.cpu_count() #uses all cpu cores available. you can change this value if you want.
 CHROME_DRIVER_PATH='./chromedriver'# path of the chromedriver for your chrome version.Make sure to download the correct chrome driver for your chrome version from https://googlechromelabs.github.io/chrome-for-testing/
